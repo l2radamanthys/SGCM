f97fa6a5fa98d0a0778dd41223a0bce214a77330
Ricardo D. Quiroga <l2radamanthys@gmail.com>
2014-04-24 10:11:18 -0300

    sqlite configuracion

--- settings.py
+++ settings.py
@@ -29,7 +29,7 @@ DATABASES = {
 #        'PASSWORD': 'inmortal',                    # Not used with sqlite3.
 #        'HOST': '127.0.0.1',                       # Set to empty string for localhost. Not used with sqlite3.
 #        'PORT': '',                                  # Set to empty string for default. Not used with sqlite3.
-    }
+#    }
 }
 
 # Local time zone for this installation. Choices can be found here:
