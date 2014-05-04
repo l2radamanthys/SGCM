96a83e29c57634a4e64e6bac318d58e6b828a6c0
Ricardo D. Quiroga <l2radamanthys@gmail.com>
2014-05-04 18:46:10 -0300

    Agregada la vista para que el medico asigne turno al paciente,el problema es que las plantillas new-turn y new-turn-date-select de las vistas del Medico no tienen asignada la plantilla base correspondiente

--- GestionTurnos/medics_views.py
+++ GestionTurnos/medics_views.py
@@ -1131,3 +1131,240 @@ def medical_prescription_pdf(request, id_pm):
 
     #html_cont = mi_template.render(Context(dict))
     #return HttpResponse(html_cont)
+
+
+def medic_new_turn_day_select(request, pac_id, month=None, year=None):
+    """
+        vista del medico para selecionar un nuevo turno paciente
+    """
+    mi_template = get_template('Medics/GestionTurnos/new-turn-date-select.html')
+    dict = generate_base_keys(request)
+
+    if True:
+        ## correcion de la fecha y de los spin para cambio de fechas ##
+        if month != None and year != None:
+            month = int(month) % 13
+            year = int(year)
+
+        else:
+            year = datetime.date.today().year
+            month = datetime.date.today().month
+
+        #hoy
+        t_day = datetime.date.today().day #dia
+        t_year = datetime.date.today().year #mes
+        t_month = datetime.date.today().month #anio
+
+        dict['month'] = month
+        dict['name_month'] = MONTHS[month-1]
+        dict['year'] = year
+
+        cal = calendar.Calendar()
+        wekends = cal.monthdayscalendar(year, month)
+
+        if month == 1:
+            dict['prev_month'] = 12
+            dict['next_month'] = 2
+            dict['prev_year'] = year - 1
+            dict['next_year'] = year
+
+        elif month == 12:
+            dict['prev_month'] = 11
+            dict['next_month'] = 1
+            dict['prev_year'] = year
+            dict['next_year'] = year + 1
+
+        else:
+            dict['prev_month'] = month - 1
+            dict['next_month'] = month + 1
+            dict['prev_year'] = year - 1
+            dict['next_year'] = year + 1
+
+        #correcion para no mostrar los meses anteriores al mes actual
+        if dict['prev_month'] < t_month and dict['prev_year'] == t_year or dict['prev_year'] < t_year:
+            dict['prev_month'] = t_month
+            dict['prev_year'] = t_year
+        ## fin correcion fechas
+        
+        dict['patient'] = User.objects.get(username=pac_id)
+        medic = UserInformation.objects.get(user=request.user)
+        dict['medic'] = medic
+        ## diferentes grupos segun patrones para armar el calendar ##
+        #dias que se atiende
+        bh = BusinessHours.objects.filter(user=medic.user)
+        wd = []
+        for day in bh:
+            wd.append(day.date - 1)
+
+        #dias no laborales
+        nwd = NonWorkingDay.objects.filter(user=medic.user, date__month=month, date__year=year)
+        _nwd = []
+        for obj in nwd:
+            _nwd.append(obj.date.day)
+
+        #dias con turnos asignados
+        _dta = []
+        dof = DayOfAttention.objects.filter(business_hour__user=medic.user, date__month=month, date__year=year)
+        for day in dof:
+            if day.status == 2: #marcado como completo
+                _dta.append(day.date - 1)
+
+        #formateo del mes
+        semanas = []
+        for week in wekends:
+            sem = []
+            i = 0
+            for day in week:
+                if day in _nwd: #es un dia no laboral? dias precancelados por el medico
+                    sem.append(CalendarDay(day,2))
+                else:
+                    if i in wd:#dias donde se atiende
+                        #estan completos los dia de atencion
+                        if day in _dta:
+                            sem.append(CalendarDay(day,3))
+
+                        #si paso la fecha de atencion se marcara como completo en la vista
+                        elif day <= t_day and t_month == month and t_year == year:
+                            sem.append(CalendarDay(day,3))
+                        else:
+                            sem.append(CalendarDay(day,1)) #hay turnos libres
+                    else:
+                        sem.append(CalendarDay(day,0)) #dias q no se atiende
+                i += 1
+            semanas.append(sem)
+        dict['wekends'] = semanas
+
+        html_cont = mi_template.render(Context(dict))
+        return HttpResponse(html_cont)
+
+    else:
+        path = request.META['PATH_INFO']
+        return HttpResponseRedirect("/restricted-access%s" %path)
+
+
+
+def medic_new_turn(request, pac_id, day, month, year):
+    mi_template = get_template('Medics/GestionTurnos/new-turn.html')
+    dict = generate_base_keys(request)
+
+    if True:
+        medic = UserInformation.objects.get(user=request.user)
+        patient = User.objects.get(id=pac_id) 
+        dict['patient'] = patient 
+        dict['medic'] = medic
+
+        if request.method == 'POST':
+            key = str(medic.user.id) + day + month + year
+            fkey = get_POST_value(request, 'key')
+            if key == fkey:#datos
+                ## consulta si es un dia valido
+                str_d = day + '-' + month + '-' + year #formatea un string como dd-mm-aaaa
+                s_day = dia_fecha(str_d) #nro de dia de la semana
+                result = BusinessHours.objects.filter(user = medic.user, date=s_day).count()
+                if result == 1:
+                    ## consulta si tiene turnos disponible el dia
+                    bh = BusinessHours.objects.get(user = medic.user, date=s_day)
+                    _date = datetime.date(int(year),int(month),int(day))
+                    is_turn = True #bandera de consulta asignacion de turno
+                    try:
+                        doa = DayOfAttention.objects.get(business_hour=bh, date=_date)
+
+                    except DayOfAttention.DoesNotExist:
+                        doa = DayOfAttention(
+                            business_hour = bh,
+                            date = _date,
+                            status = 0,
+                            number_of_turns = 0,
+                            current_end_time = bh.start_time
+                        )
+                        doa.save()
+
+                    if doa.number_of_turns > doa.business_hour.number_of_turns():
+                        doa.status = 2 #esta completo de antemano no puede solicitar turno
+                        is_turn = False #ya no se puede asignar turno
+
+                    elif doa.number_of_turns == (doa.business_hour.number_of_turns() - 1):
+                        doa.status = 2 #ulimo turno que se asignara
+                        doa.number_of_turns += 1
+
+                    else:
+                        doa.status = 1
+                        doa.number_of_turns += 1
+
+                    _start = doa.current_end_time
+                    _end = doa.get_next_current_end_time()
+                    doa.current_end_time = doa.get_next_current_end_time()
+
+                    ## debug
+                    #dict['test'] = bh.number_of_turns_bh(bh)
+                    #dict['test'] = doa.number_of_turns
+                    #dict['test'] = doa.get_next_current_end_time()
+                    #dict['test'] = str(doa.number_of_turns) + " - "
+                    #dict['test'] += str(_start) + "  - "
+                    #dict['test'] += str(_end)
+
+
+                    if is_turn: #consulta bandera de registracion de turno
+                        dict['not_errors'] = True
+                        try:
+                             #lanza un error en caso que se haya registrado un turno previamente en la misma fecha
+                            #con el mismo especialista
+                            turn = Turn.objects.get(medic=medic.user, patient=patient, day = doa)
+                            if turn.status == 3: #reactivar turno previamente cancelado
+                                turn.status = 0 #cambia a pendiente
+                                turn.save()
+                                dict['turn'] = turn
+
+                            else:
+                                dict['errors'] = 'No se pudo registrar el turno, por que ya posee un turno registrado esta fecha con dicho especialista'
+                                dict['not_errors'] = False
+
+                        except Turn.DoesNotExist:
+                            #no hay un turno previo registrado
+                            turn = Turn(
+                                day = doa, #dia de atencion al cual esta relacionado
+                                medic = medic.user,
+                                patient = patient, #se supone que el paciente es el user actual logueado
+                                start = _start,
+                                end = _end,
+                                status = 0,
+                                observation = '',
+                                number = doa.number_of_turns
+                            )
+                            turn.save()
+                            doa.save()
+                            dict['turn'] =turn
+
+                    else:
+                        dict['not_errors'] = False
+                        dict['errors'] = 'Lo sentimos pero la fecha' + str(doa.date) + 'no tiene turnos disponibles'
+
+                else:
+                    dict['not_errors'] = False
+                    dict['errors'] = 'El dia solicitado es invalido'
+
+            else:
+                dict['not_errors'] = False
+                dict['errors'] = 'La clave de comprobacion no coincide'
+
+            pass
+        else:
+            dict['show_form'] = True
+            dict['key'] = str(medic.user.id) + day + month + year
+            dict['date_str'] = day + ' de ' +  MONTHS[int(month)-1] + ' de ' + year
+
+        html_cont = mi_template.render(Context(dict))
+        return HttpResponse(html_cont)
+
+    else:
+        path = request.META['PATH_INFO']
+        return HttpResponseRedirect("/restricted-access%s" %path)
+
+
+
+def patient_set_medic_consulation(request, med_id):
+    """
+        Envia una consulta online a un medico, lo que en realidad es
+        un mensaje
+    """
+    pass
